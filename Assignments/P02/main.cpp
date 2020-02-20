f06784bb7484e68d7176d60dfbaceb6d746b88c6
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-02-19 19:02:58 -0600

    main.cpp

--- Assignments/P02/main.cpp
+++ Assignments/P02/main.cpp
@@ -2,47 +2,47 @@
 #include <time.h>
 #include <chrono> 
 #include <vector>
-#include "Timer.hpp"                 // need to grab a copy from resources folder
-#include "JsonFacade.hpp"            // need to grab a copy from resources folder
-#include "Dictionary.cpp"            // need to grab a copy from resources folder
-#include "mygetch.hpp"
+#include "Timer.hpp"                                              // need to grab a copy from resources folder
+#include "JsonFacade.hpp"                                         // need to grab a copy from resources folder
+#include "Dictionary.cpp"                                         // need to grab a copy from resources folder
+#include "mygetch.hpp"                                            // need to grab a copy from resources folder
 using namespace std;
 
 int main(){
     Timer T;
     Dictionary D;
-    JsonFacade J("dict_w_defs.json");       // create instance of json class               
+    JsonFacade J("dict_w_defs.json");                             // create instance of json class               
     string key;
-    string defs;                     // key variable to access json object
-    int count=0;                     // number of character that input
-    vector<string> keys = J.getKeys();   //create a vector to store the words from infile
+    string defs;                                                  // key variable to access json object
+    int count=0;                                                  // number of character that input
+    vector<string> keys = J.getKeys();                            //create a vector to store the words from infile
     cout << "Yuankai MA\n";
     cout<<"Number of the words in the infile is " <<keys.size()<<endl;
-    for(int i = 2; i < keys.size(); i++){    //store all words and defs into linked list
+    for(int i = 2; i < keys.size(); i++){                         //store all words and defs into linked list
       key = (J.getKey(i));
       defs = (J.getValue(key));
       D.Add(key,defs);
     }
 
-    char k;             // holder for character being typed
-    string Word = "";   // var to concatenate letters to
+    char k;                                                       // holder for character being typed
+    string Word = "";                                             // var to concatenate letters to
     cout<<"Type keys and watch what happens. Type capital Z to quit."<<endl;
     // While capital Z is not typed keep looping
-    while ((k = getch()) != 'Z') {
-        Word += k;          // append char to word
-        if((int)k != 10){   // if k is not a space print it
+    while ((k = getch()) != 'Z') {                                
+        Word += k;                                                // append char to word
+        if((int)k != 10){                                         // if k is not a space print it
             cout << "Key: " << k << endl;
             cout << "Word: " << Word << endl;
             while(Word[count] != '\0'){
               count++;
             }
-            T.Start();               //calculate time start
+            T.Start();                                            //calculate time start
             D.findWords(Word,count);
-            T.End();                 //calculate time stop
+            T.End();                                              //calculate time stop
             double s = T.Seconds();
             cout << D.wordNum() << " words is found in " << s << " seconds\n";
-            D.tenWords();            //cout first up to 10 words
-            D.Delete();              //delete the vector
+            D.tenWords();                                         //cout first up to 10 words
+            D.Delete();                                           //delete the vector
         }
 
         // hitting enter sets word back to empty
