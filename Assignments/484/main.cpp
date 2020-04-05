5034b2ad6af013df358a14e72b57920914e5c1f6
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-04-05 18:24:20 -0500

    Update main.cpp

--- Assignments/484/main.cpp
+++ Assignments/484/main.cpp
@@ -10,17 +10,18 @@ vector<int> Vec;
 map<int, int> Map;
 
 int main() {
-  int n;
-  while (cin >> n) {
-    if (Map[n] == 0) {
-      Vec.push_back(n);
-      Map[n]++;
-    }
-    else
-      Map[n]++;
-  }
-  for (int i = 0; i < Vec.size(); i++) {
-    cout << Vec[i] << " " << Map[Vec[i]] << endl;
-  }
-  return 0;
+	int n;
+	while (cin >> n) {
+		if (Map[n] == 0) {
+			Vec.push_back(n);
+			Map[n]++;
+		}
+		else {
+			Map[n]++;
+		}
+	}
+	for (int i = 0; i < Vec.size(); i++) {
+		cout << Vec[i] << " " << Map[Vec[i]] << endl;
+	}
+	return 0;
 }
